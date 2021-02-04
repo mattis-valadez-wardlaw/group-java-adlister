f57672dabb388829bdaf6eae4fb2591743e9f028
Caleb Wardlaw <calebwardlaw@gmail.com>
2021-02-04 13:39:38 -0600

    switching branches

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -51,6 +51,25 @@ public class MySQLUsersDao implements Users {
         }
     }
 
+    @Override
+    public Long update(User user) {
+        String query = "UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2,user.getUsername());
+            stmt.setString(3,user.getPassword());
+            stmt.setLong(4,user.getId());
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error editing profile");
+        }
+    }
+
+
     private User extractUser(ResultSet rs) throws SQLException {
         if (! rs.next()) {
             return null;
