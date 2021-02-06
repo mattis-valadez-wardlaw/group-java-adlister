a30a9831f43fbbee19a5fde32754d7ea0fce69c8
Amber Mattis <amber.mattis2@gmail.com>
2021-02-06 10:15:14 -0600

    Feat: added error messages for passwords confirmation

--- src/main/java/com/codeup/adlister/controllers/AdEditServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdEditServlet.java
@@ -14,20 +14,33 @@ import java.io.IOException;
 @WebServlet(name="AdEditServlet", urlPatterns = "/ads/edit")
 public class AdEditServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String query = request.getQueryString();
-        Long id = Long.valueOf(query.substring(3));
-        //Use Dao to get current ad
+        try {
+            String query = request.getQueryString();
+            Long id = Long.valueOf(query.substring(3));
+            //Use Dao to get current ad
 
+            Ad currentAd = DaoFactory.getAdsDao().singleAd(id);
 
+            //Set this sessions attribute to current ad
+            request.getSession().setAttribute("ad", currentAd);
 
-        Ad currentAd = DaoFactory.getAdsDao().singleAd(id);
+            request.getRequestDispatcher("/WEB-INF/ads/edit.jsp").forward(request, response);
 
+        }
 
-
-        //Set this sessions attribute to current ad
-        request.getSession().setAttribute("ad", currentAd);
-
-        request.getRequestDispatcher("/WEB-INF/ads/edit.jsp").forward(request, response);
+            catch (Exception e) {
+                response.sendRedirect("/redirect");
+            }
+//        String query = request.getQueryString();
+//        Long id = Long.valueOf(query.substring(3));
+//        //Use Dao to get current ad
+//
+//        Ad currentAd = DaoFactory.getAdsDao().singleAd(id);
+//
+//        //Set this sessions attribute to current ad
+//        request.getSession().setAttribute("ad", currentAd);
+//
+//        request.getRequestDispatcher("/WEB-INF/ads/edit.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
