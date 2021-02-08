e3b3b668d7dc33c8ba234d54f50271cd5e140655
Caleb Wardlaw <calebwardlaw@gmail.com>
2021-02-08 10:49:53 -0600

    annotated User.Java to show paths of models

--- src/main/java/com/codeup/adlister/models/User.java
+++ src/main/java/com/codeup/adlister/models/User.java
@@ -10,19 +10,27 @@ public class User {
 
     public User() {}
 
+    /////////////////////
+    //RegisterServlet//
+    ////////////////////
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         setPassword(password);
     }
 
+    /////////////////////
+    //MySQLUsersDao//
+    ////////////////////
     public User(long id, String username, String email, String password) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
     }
-
+    /////////////////////
+    //ProfileEditServlet//
+    ////////////////////
     public User(Long id, String username, String email, String password) {
         this.id = id;
         this.username = username;
